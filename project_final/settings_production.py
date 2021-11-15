ce829c1e582e9d65b631673deeeb26ac8141e222
Raul Aviles <finbravilesr@gmail.com>
2021-11-15 11:29:21 -0500

    Initialize Django project

--- project_final/settings_production.py
+++ project_final/settings_production.py
@@ -1,8 +1,7 @@
 import dj_database_url
 from decouple import config
 
-DEBUG = config('DJANGO_DEBUG', default=True, cast=bool)
-SECRET_KEY = config('SECRET_KEY', default='Casa1234')
+
 
 DATABASES = {
     'default': dj_database_url.config(
